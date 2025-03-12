8f328f15502ac51b13097a20e5c48c381ba09579
MLauraCronemberger <maria.laura.cronemberger@gmail.com>
2025-03-12 09:11:46 -0300

    Funcao para consertar o valor

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,38 @@
-let dolar = 5.81
+let dolar = 5.81;
 
-let usdInput = document.querySelector("#usd")
-let brlInput = document.querySelector("#brl")
+let usdInput = document.querySelector("#usd");
+let brlInput = document.querySelector("#brl");
 
-usdInput.addEventListener("keyup", () => {
+usdInput.addEventListener("keyup", () => {});
 
-})
+brlInput.addEventListener("keyup", () => {});
 
-brlInput.addEventListener("keyup", () => {
-    
-})
+//Vou setar o valor do dollar como MIL, e dps a pessoa altera para oq ela quiser
+usdInput.value = "1000,00";
+convert("usd-to-brl");
+
+//Funções
+
+function formatCurrency(value) {
+  //ajustar o valor
+  //utilizar funcao de formatar
+  //retorna o valor formatado
+}
+
+function fixValue(value) {
+  let fixedValue = value.replace(",", ".");
+  let floatValue = parseFloat(fixedValue);
+
+  if (floatValue == NaN0) {
+    floatValue = 0;
+  }
+
+  return floatValue;
+}
+function convert(type) {
+  if (type == "usd-to-brl") {
+  }
+
+  if (type == "brl-to-usd") {
+  }
+}

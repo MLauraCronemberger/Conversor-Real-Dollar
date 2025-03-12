09cf15adf24a2405b727dc3bffee7f55c8a051df
Maria Laura Cronemberger <maria.laura.cronemberger@gmail.com>
2025-03-11 23:50:46 -0300

    Inicio do Projeto

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,12 @@
-//armazenar em uma variável cada um dos inputs do nosso html
-let usdInput= document.querySelector("#usd")
+let dolar = 5.81
 
+let usdInput = document.querySelector("#usd")
 let brlInput = document.querySelector("#brl")
 
-//agora vc consegue adicionar ações dessa sua variável
-//evento  de apertar e soltar a tecla
-
-usdInput.addEventListener("keyup", ()=> {
-    console.log("Vish, vc apertou num lugar bacsns")
+usdInput.addEventListener("keyup", () => {
 
 })
 
-brlInput.addEventListener("keydown", ()=> {
-    console.log("Vish, vc apertou num lugar bacsns")
+brlInput.addEventListener("keyup", () => {
     
 })
